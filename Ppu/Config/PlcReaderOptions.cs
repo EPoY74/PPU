446fdd5158cf6608eb54072de2d86974c7c8d860
EvgeniiPetrovIceBookWin <epoy74@gmail.com>
2026-04-03 22:18:20 +1000

    feat(config): add PlcReaderOptions

---
+++ Ppu/Config/PlcReaderOptions.cs
@@ -0,0 +1,15 @@
+using Ppu.Domain;
+
+namespace Ppu.Config;
+
+public sealed class PlcReaderOptions
+{
+    public string Host { get; init; } = "127.0.0.1",
+    public int Port { get; init; } = 502;
+    public byte UnitId { get; init; } = 1;
+    public ModbusFunctionCode FunctionCode { get; init; } = ModbusFunctionCode.ReadHoldingRegisters;
+    public ushort StartAddress { get; init; } = 0;
+    public ushort RegisterCount { get; init; } = 2;
+    public int PollIntervalSecond { get; init; } = 5;
+
+}
