c971fcc1b27cd48508639c50615c938e1ba10500
EvgeniiPetrovIceBookWin <epoy74@gmail.com>
2026-04-03 20:44:37 +1000

    feat(domain): add RawReadResult

---
+++ Ppu/Domain/RawReadResult.cs
@@ -0,0 +1,11 @@
+namespace Ppu.Domain;
+
+public sealed class RawReadResult
+{
+    public DateTimeOffset TimestampUtc { get; init; }
+    public bool IsSuccess { get; init; }
+    public string? ErrorMessage { get; init; }
+    public ushort[] Registers { get; init; } = Array.Empty<ushort>();
+    public int DurationsMs { get; init; }
+
+}
