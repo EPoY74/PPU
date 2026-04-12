90d4a630ae771f050a86b6092ba2fbdf2d3c6887
EvgeniiPetrovIceBookWin <epoy74@gmail.com>
2026-04-12 14:29:59 +1000

    feat(domain):  add FunctionCode into RawReadResult

--- Ppu/Domain/RawReadResult.cs
+++ Ppu/Domain/RawReadResult.cs
@@ -5,6 +5,7 @@ public sealed class RawReadResult
     public DateTimeOffset TimestampUtc { get; init; }
     public bool IsSuccess { get; init; }
     public string? ErrorMessage { get; init; }
+    public int FunctionCode { get; init; } = (int)ModbusFunctionCode.ReadHoldingRegisters;
     public ushort[] Registers { get; init; } = [];  //Array.Empty<ushort>();
     public int DurationsMs { get; init; }
 
