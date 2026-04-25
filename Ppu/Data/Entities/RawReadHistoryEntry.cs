7119155e82fd44ad3a82278b7880f7faabfd0aaf
EvgeniiPetrovIceBookWin <epoy74@gmail.com>
2026-04-26 01:02:34 +1000

    feat(db): define max string lengths for error messages and register JSON fields

--- Ppu/Data/Entities/RawReadHistoryEntry.cs
+++ Ppu/Data/Entities/RawReadHistoryEntry.cs
@@ -1,4 +1,6 @@
-﻿namespace Ppu.Data.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ppu.Data.Entities;
 
 public sealed class RawReadHistoryEntry
 {
@@ -6,11 +8,13 @@ public sealed class RawReadHistoryEntry
     public Guid AppRunId { get; set; }
     public DateTimeOffset TimeStampUtc { get; set; }
     public bool IsSuccess { get; set; }
+    [StringLength(512)]
     public string? ErrorMessage {  get; set; }
     public int FunctionCode { get; set; }
     public ushort StartAddress { get; set; }
     public ushort RegisterCouunt { get; set; }
     public ushort StopAddress { get; set; }
+    [StringLength(270)]
     public string RegistersJson { get; set; } = "[]";
     public int Duration { get; set; }
 }
