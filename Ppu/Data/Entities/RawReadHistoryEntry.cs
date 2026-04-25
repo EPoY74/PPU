a4c95fda0b90d7aacdf08e22f44c8b2880d68295
EvgeniiPetrovIceBookWin <epoy74@gmail.com>
2026-04-25 22:34:56 +1000

    feat(bd): add RawReadHistoryEntry

---
+++ Ppu/Data/Entities/RawReadHistoryEntry.cs
@@ -0,0 +1,16 @@
+﻿namespace Ppu.Data.Entities;
+
+public sealed class RawReadHistoryEntry
+{
+    public long Id { get; set; }
+    public Guid AppRunId { get; set; }
+    public DateTimeOffset TimeStampUtc { get; set; }
+    public bool IsSuccess { get; set; }
+    public string? ErrorMessage {  get; set; }
+    public int FunctionCode { get; set; }
+    public ushort StartAddress { get; set; }
+    public ushort RegisterCouunt { get; set; }
+    public ushort StopAddress { get; set; }
+    public string RegisterJson { get; set; } = "[]";
+    public int Duration { get; set; }
+}
