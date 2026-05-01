f64046a4ab73d25d28b4d21a52ab1f5a1c45e0c5
EvgeniiPetrovIceBookWin <epoy74@gmail.com>
2026-05-01 16:47:54 +1000

    feat(domain): add StartAddress to RawReadResult and populate in reader/writer

--- Ppu/Domain/RawReadResult.cs
+++ Ppu/Domain/RawReadResult.cs
@@ -5,8 +5,8 @@ public sealed class RawReadResult
     public DateTimeOffset TimestampUtc { get; init; }
     public bool IsSuccess { get; init; }
     public string? ErrorMessage { get; init; }
+    public ushort StartAddress { get; init; }
     public int FunctionCode { get; init; } = (int)ModbusFunctionCode.ReadHoldingRegisters;
-    public ushort StartAddress { get; init; } = 0;
     public ushort[] Registers { get; init; } = [];  //Array.Empty<ushort>();
     public int DurationsMs { get; init; }
 
